a24cba5943b72468deb2217c05bdd1f28b10530d
George Hou <George Hou>
2020-01-14 16:52:46 -0800

    refactor code, update test

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,6 +1,8 @@
 '''
 Linked List hash table key/value pair
 '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
@@ -17,13 +19,13 @@ class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-        self.entries = 0 # Track entries to auto resize
-        self.start_capacity = capacity
+        # 2nd Stretch Goal
+        self.entries = 0  # Track entries to auto resize
+        self.start_capacity = capacity # Save initial capacity
 
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
-
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
         return hash(key)
@@ -31,13 +33,21 @@ class HashTable:
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
-
-        OPTIONAL STRETCH: Research and implement DJB2
+        https://stackoverflow.com/a/13809282/11304328
+        Start from Magic Constant 5831
+        1. odd number
+        2. prime number
+        3. deficient number
+        4. 001/010/100/000/101 b
         '''
+        # Cast the key to a string
+        str_key = str(key)
+        # Start from Magic Constant 5831
         djb2_hash = 5381
-        for c in key:
-            djb2_hash = (djb2_hash * 33) + ord(c)
-
+        # Bit-shift and sum value for each character
+        for char in str_key:
+            # djb2_hash = ((djb2_hash << 5) + djb2_hash) + ord(char)
+            djb2_hash = (djb2_hash * 33) + ord(char)
         return djb2_hash
 
     def _hash_mod(self, key):
@@ -50,9 +60,7 @@ class HashTable:
     def insert(self, key, value):
         '''
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Fill this in.
         '''
         # double in size automatically when hashtable
@@ -64,15 +72,12 @@ class HashTable:
         current = self.storage[index]
         # Insert value
         if current:
-            while current:
-                if key == current.key:
-                    current.value = value
-                    return
-                if current.next:
-                    current = current.next
-                else:
-                    break
-            current.next = LinkedPair(key, value)
+            while current.key != key and current.next:
+                current = current.next
+            if current.key == key:
+                current.value = value
+            else:
+                current.next = LinkedPair(key, value)
         else:
             self.storage[index] = LinkedPair(key, value)
 
@@ -81,9 +86,7 @@ class HashTable:
     def remove(self, key):
         '''
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Fill this in.
         '''
         index = self._hash_mod(key)
@@ -91,29 +94,27 @@ class HashTable:
 
         if key == current.key:
             self.storage[index] = self.storage[index].next
+            self.entries -= 1
 
         while current.next:
             if key == current.next.key:
                 current.next = current.next.next
+                self.entries -= 1
                 return
             else:
                 current = current.next
-
-        self.entries -= 1
 
         # half in size when it shrinks past a load factor of 0.2
         # This should only occur if the HashTable
         # has been resized past the initial size
         if ((self.start_capacity != self.capacity) &
-              (self.entries / self.capacity < .2)):
+                (self.entries / self.capacity < .2)):
             self.resize(.5)
 
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Fill this in.
         '''
         index = self._hash_mod(key)
@@ -130,16 +131,13 @@ class HashTable:
         '''
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
-
         Fill this in.
         '''
-        prev_capacity = self.capacity
         self.capacity = int(self.capacity * factor)
         prev_storage = self.storage
         self.storage = [None] * self.capacity
 
-        for index in range(prev_capacity):
-            current = prev_storage[index]
+        for current in prev_storage:
             while current:
                 self.insert(current.key, current.value)
                 current = current.next
@@ -149,7 +147,7 @@ class HashTable:
 
 
 if __name__ == "__main__":
-    ht = HashTable(9)
+    ht = HashTable(10)
 
     ht.insert("line_1", "Tiny hash table")
     ht.insert("line_2", "Filled beyond capacity")
@@ -173,3 +171,14 @@ if __name__ == "__main__":
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
     print(ht.retrieve("line_3"))
+
+    print(ht._hash_mod("key-9"))
+    print(ht._hash_mod("key-8"))
+    print(ht._hash_mod("key-7"))
+    print(ht._hash_mod("key-6"))
+    print(ht._hash_mod("key-5"))
+    print(ht._hash_mod("key-4"))
+    print(ht._hash_mod("key-3"))
+    print(ht._hash_mod("key-2"))
+    print(ht._hash_mod("key-1"))
+    print(ht._hash_mod("key-0"))
